8ee8a41c80a143d3ad380714c3e6d6dd7adf0d05
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-06-20 11:57:28 +0000

    feat: :construction: Se continuo el codigo

--- Dia1/ModeloFisico.sql
+++ Dia1/ModeloFisico.sql
@@ -2,8 +2,8 @@
 -- # Dia 1 # Normalizacion #
 -- #########################
 
-create database dia1
-use dia1
+create database dia1;
+use dia1;
 
 create table medico(
      id int primary key not null,
@@ -18,7 +18,14 @@ create table medico(
      colegiado int not null,
      tipo enum('médico titular','médico interino','médico sustituto')
 );
-INSERT INTO medico VALUES (1,'Juliana Mendoza','Cra.13 Calle 20',2468097531,'vulnerable','Metropolitana','12ls',7587,98765,34,'médico interino')
+INSERT INTO medico VALUES (1,'Juliana Mendoza','Cra.13 Calle 20',246809751,'vulnerable','Metropolitana','12ls',7587,98765,34,'médico interino');
+INSERT INTO medico VALUES (2,'Einstein','Calle 10',943167852,'viva','Metropolitana','13g',932,123,45,'médico titular');
+INSERT INTO medico VALUES (3,'Guillermo Gomez','Pan azucar',546897213,'s','Metropolitana','46h');
+INSERT INTO medico VALUES ();
+INSERT INTO medico VALUES ();
+INSERT INTO medico VALUES ();
+INSERT INTO medico VALUES ();
+INSERT INTO medico VALUES ();
 
 create table sustituciones(
      id_medico int,
@@ -40,7 +47,7 @@ create table empleado(
      seguridad_social int not null,
      tipo enum('ATS','ATS de zona','auxiliar de enfermería','celador','administrativo')
 );
-INSERT INTO medico VALUES
+-- INSERT INTO medico VALUES
 
 create table paciente(
      id int primary key not null,
@@ -67,7 +74,7 @@ create table vacaciones(
      fecha_inicio date not null,
      fecha_fin date not null,
      foreign key(id_empleado) references medico(id),
-     foreign key(id_empleado) references empleado(id),
+     foreign key(id_empleado) references empleado(id)
 );
 
 --     ##### Consultas
@@ -76,18 +83,18 @@ create table vacaciones(
 select m.nombre as Medico, count(p.id) as Num_Pacientes
 from medico m
 left join paciente p on p.id_medico = m.id
-group by (m.id)
+group by (m.id);
 
 --  2. Total de días de vacaciones planificadas y disfrutadas por cada empleado
 select e.nombre, sum(datediff(v.fecha_inicio, v.fecha_fin)) as Vacaciones
 from vacaciones v
 inner join empleado e on e.id = v.id_empleado
 inner join medico m on m.id = v.id_empleado
-group by v.id_empleado
+group by v.id_empleado;
 
 -- 3. Médicos con mayor cantidad de horas de consulta en la semana
-select m.nombre, sum(TIMESTAMPDIFF(hour,c.hora_inicio,c.hora_fin)) as Horas
-from consultas c
+-- select m.nombre, sum(TIMESTAMPDIFF(hour,c.hora_inicio,c.hora_fin)) as Horas
+-- from consultas c
 
 
 -- 4. Número de sustituciones realizadas por cada médico sustituto
