7bc2e6b1d77c8314520855280d1fbf0fb464e9e5
Catalina <150844717+Ctlna@users.noreply.github.com>
2024-06-21 14:08:46 +0000

    feat: :construction: Continuación del codigo

--- Dia2/Actividad2.sql
+++ Dia2/Actividad2.sql
@@ -119,15 +119,53 @@ from empleado;
 -- identificador de los empleados junto al nif, pero el nif deberá aparecer en dos columnas,
 -- una mostrará únicamente los dígitos del nif y la otra la letra.
 delimiter //
-create procedure separa_nif()
+create function separa_numeros_nif(nif varchar(9))
+returns varchar(9) deterministic
 begin
-     select id,
-          left(nif,length(nif)-1) as Numeros
-          right(nif,1) as Letras
-     from empleado;
+     declare ctrNumber varchar(50);
+     declare finText text default ' ';
+     declare sChar varchar(2);
+     declare inti integer default 1;
+     if length(nif) > 0 then
+		while(inti <= length(nif))  do
+			set sChar= SUBSTRING(nif,inti,1);
+			set ctrNumber= FIND_IN_SET(sChar,'0,1,2,3,4,5,6,7,8,9');
+			if ctrNumber = 0 then
+				set finText=CONCAT(finText,sChar);
+			else set finText=CONCAT(finText,'');
+			end if;
+            set inti=inti+1;
+        end while;
+        return finText;
+	else return '';
+    end if;
 end //
 delimiter ;
-call separa_nif();
+delimiter //
+create function separa_letras_nif(nif varchar(9))
+returns varchar(9) deterministic
+begin
+     declare ctrNumber varchar(50);
+     declare finText text default ' ';
+     declare sChar varchar(2);
+     declare inti integer default 1;
+     if length(nif) > 0 then
+		while(inti <= length(nif))  do
+			set sChar= SUBSTRING(nif,inti,1);
+			set ctrNumber= FIND_IN_SET(sChar,'q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,ñ,z,x,c,v,b,n,m');
+			if ctrNumber = 0 then
+				set finText=CONCAT(finText,sChar);
+			else set finText=CONCAT(finText,'');
+			end if;
+            set inti=inti+1;
+        end while;
+        return finText;
+	else return '';
+    end if;
+end //
+delimiter ;
+select separa_numeros_nif(nif),separa_letras_nif(nif)
+from empleado;
 
 
 -- nombre de cada departamento y el presupuesto actual del que dispone. Para calcular
@@ -140,7 +178,7 @@ create function calcula_presupuesto(presupuesto DOUBLE,gastos DOUBLE)
 returns DOUBLE deterministic
 begin
 	 return presupuesto - gastos;
-end//
+end //
 delimiter ;
 select nombre, calcula_presupuesto(presupuesto,gastos) as Total
 from departamento;
@@ -151,7 +189,7 @@ create function calcula_presupuesto(presupuesto DOUBLE,gastos DOUBLE)
 returns DOUBLE deterministic
 begin
 	declare total double;
-        return presupuesto - gastos;
+	return presupuesto - gastos;
 end//
 delimiter ;
 select nombre, calcula_presupuesto(presupuesto,gastos) as Total
@@ -161,14 +199,127 @@ order by Total asc;
 -- nombre de todos los departamentos ordenados de forma ascendente.
 select nombre
 from departamento
-order by Total asc;
+order by nombre asc;
 
 -- nombre de todos los departamentos ordenados de forma descendente.
 select nombre
 from departamento
-order by Total desc;
+order by nombre desc;
 
+-- apellidos y el nombre de todos los empleados, ordenados de forma alfabética tendiendo en cuenta 
+-- en primer lugar sus apellidos y luego su nombre.
+select apellido1,apellido2,nombre
+from empleado
+order by apellido1,apellido2,nombre asc;
 
+-- nombre y el presupuesto, de los 3 departamentos que tienen mayor presupuesto.
+drop function top_presupuesto;
+delimiter //
+	create function top_presupuesto( )
+    returns double deterministic
+    begin
+		declare Presupuesto double;
+		select presupuesto as Presupuesto 
+        from departamento order by presupuesto desc limit 1
+        into Presupuesto;
+        return Presupuesto;
+    end //
+delimiter ;
+select top_presupuesto();
+
+select nombre as Departamento, presupuesto as Presupuesto
+from departamento
+order by presupuesto desc
+limit 3;
+
+-- nombre y el presupuesto, de los 3 departamentos que tienen menor presupuesto.
+select nombre as Departamento, presupuesto as Presupuesto
+from departamento
+order by presupuesto asc
+limit 3;
+
+-- nombre y el gasto, de los 2 departamentos que tienen mayor gasto.
+select nombre as Departamento, gastos as Gasto
+from departamento
+order by gastos desc
+limit 2;
+
+-- nombre y el gasto, de los 2 departamentos que tienen menor gasto.
+select nombre as Departamento, gastos as Gasto
+from departamento
+order by gastos asc
+limit 2;
+
+--                 ## Consultas resumen
+
+-- Calcula la suma del presupuesto de todos los departamentos.
+delimiter //
+create function suma_presupuesto()
+returns double deterministic
+begin
+	declare Total double;
+    select sum(presupuesto) as Total_suma from departamento into Total;
+    return Total;
+end //
+delimiter ;
+select suma_presupuesto();
+
+-- Calcula la media del presupuesto de todos los departamentos.
+delimiter //
+create function promedio_presupuesto()
+returns double deterministic
+begin
+	declare Total double;
+    select avg(presupuesto) as Total_Promedio from departamento into Total;
+    return Total;
+end //
+delimiter ;
+select promedio_presupuesto();
+
+-- Calcula el valor mínimo del presupuesto de todos los departamentos.
+delimiter //
+create function minimo_presupuesto()
+returns double deterministic
+begin
+	declare Total double;
+    select min(presupuesto) as Valor_Minimo from departamento into Total;
+    return Total;
+end //
+delimiter ;
+select minimo_presupuesto();
+
+-- Calcula el nombre del departamento y el presupuesto que tiene asignado, del departamento con menor 
+-- presupuesto.
+
+drop function minimo;
+
+delimiter //
+create function minimo()
+returns double deterministic
+begin
+	declare Total double;
+    declare Departamento double;
+    select nombre into Departamento from departamento;
+    select presupuesto into Total from departamento;
+    if presupuesto = 0 then
+		return concat(Departamento, ' tiene ',Total);
+    end if;
+    return Concat(Total);
+end //
+delimiter ;
+select minimo();
+
+-- Calcula el valor máximo del presupuesto de todos los departamentos.
+-- Calcula el nombre del departamento y el presupuesto que tiene asignado, del departamento con mayor presupuesto.
+-- Calcula el número total de empleados que hay en la tabla empleado.
+-- Calcula el número de empleados que no tienen NULL en su segundo apellido.
+-- Calcula el número de empleados que hay en cada departamento. Tienes que devolver dos columnas, 
+-- una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+-- Calcula el nombre de los departamentos que tienen más de 2 empleados. El resultado debe tener dos 
+-- columnas, una con el nombre del departamento y otra con el número de empleados que tiene asignados.
+-- Calcula el número de empleados que trabajan en cada uno de los departamentos. El resultado de esta 
+-- consulta también tiene que incluir aquellos departamentos que no tienen ningún empleado asociado.
+-- Calcula el número de empleados que trabajan en cada unos de los departamentos que tienen un presupuesto mayor a 200000 euros.
 
 
 -- Desarrollado por Catalina Mulford / ID 1.097.490.150
